a934fdbe07b14793e41831d43a7bc2d2c6364bb1
Devenko <48014225+Devenko@users.noreply.github.com>
2019-04-23 13:32:50 +0300

    Переделал Lab1_5

--- Training/Lab1_5/Program.cs
+++ Training/Lab1_5/Program.cs
@@ -68,29 +68,29 @@ namespace Lab1_5
             //Implement positive integer variable input
             int q;
             Console.Write("Enter the number: ");
-            q = Int32.Parse( Console.ReadLine());
-            
+            q = Int32.Parse(Console.ReadLine());
+
             //Present it like binary string
             //   For example, 4 as 100
-            String number = Convert.ToString(q,2);
+            String number = Convert.ToString(q, 2);
             Console.WriteLine($"Number = {q} in binary = {number}");
-           
+
             //Use modulus operator to obtain the remainder  (n % 2) 
             //and divide variable by 2 in the loop
             Console.WriteLine("The remainder of dividing " + q + " by 2: " + q % 2);
-            
+
             //Use the ToCharArray() method to transform string to chararray
             //and Array.Reverse() method
             Array numberArray = number.ToCharArray();
-            
-            
-            Console.WriteLine("\n"+new string('-', 100));
+
+
+            Console.WriteLine("\n" + new string('-', 100));
             Array.Reverse(numberArray);
             foreach (var VARIABLE in numberArray)
             {
                 Console.Write(VARIABLE);
             }
-            Console.WriteLine($"\nf({q}) = {q} ({number})");           
+            Console.WriteLine($"\nf({q}) = {q} ({number})");
         }
         #endregion
 
@@ -99,47 +99,58 @@ namespace Lab1_5
         {
             //Declare int and string variables for decimal and binary presentations
             int x, y;
-            string xStr, yStr;
+            string xStr = "", yStr = "";
             //Implement two positive integer variables input
             Console.Write("Enter the first number: ");
             x = Int32.Parse(Console.ReadLine());
             Console.Write("Enter the second number: ");
             y = Int32.Parse(Console.ReadLine());
+
+            if (x < 0)
+            {
+                x *= -1;
+            }
+
+            if (y < 0)
+            {
+                y *= -1;
+            }
+            Console.WriteLine($"X = {x}; Y = {y}.");
             //To present each of them in the form of unary string use for loop
-            xStr = Convert.ToString(x,2);
-            yStr = Convert.ToString(y, 2);
+            //xStr = Convert.ToString(x, 2);
+            //yStr = Convert.ToString(y, 2);
             //Use concatenation of these two strings 
             //Note it is necessary to use some symbol ( for example “#”) to separate
             char sep = '#';
+
+            for (int i = 0; i < x; i++)
+            {
+                xStr += "1";
+            }
+            for (int i = 0; i < y; i++)
+            {
+                yStr += "1";
+            }
             string result = xStr + sep + yStr;
+            
             foreach (var VARIABLE in result)
             {
                 Console.Write(VARIABLE);
             }
-            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("\n" + new string('+', 30));
 
-            //int ind = result.Length;
-            //char[] chTrim = {'1', '0'};
-
-            /*Не получается обрезать строку            
-             */
-
-
-            for (int i = 0; i < result.Length; i++)
+            while ((result[result.Length - 1] != '#') && (result[0] != '#'))
             {
-                if (result.StartsWith("#") | (result.EndsWith("#")))
-                {
-                    result = result.Substring(0, result.Length-1);
-                    result = result.Remove(0,1);
-                    
-                    //result = result.Substring(ind, 1);
-                    //result = result.Substring(0, ind);
-                    foreach (var VARIABLE in result)
-                    {
-                        Console.Write(VARIABLE);
-                    }
-                }
-            }Console.WriteLine(new string('-', 30));
+                result = result.Remove(result.Length - 1, 1);
+                result = result.Remove(0, 1);
+                Console.WriteLine(result);
+            }
+            
+            foreach (var VARIABLE in result)
+            {
+                Console.Write(VARIABLE);
+            }
+            Console.WriteLine("\n" + new string('-', 30));
 
             foreach (var VARIABLE in result)
             {
@@ -149,12 +160,25 @@ namespace Lab1_5
             //Check the numbers on the equality 0
             //Realize the  algorithm for replacing '1#1' to '#' by using the for loop 
             //Delete the '#' from algorithm result
-            if ((x | y) != 0)
+            int index = 0;
+            int w = 0;
+
+            for (int i = 0; i < result.Length; i++)
             {
-
+                if (result[i] == sep)
+                {
+                    index = result.IndexOf(sep);
+                    Console.WriteLine($"\nIndex = {index}");
+                    result = result.Remove(index, 1);
+                    Console.WriteLine(result);
+                }
             }
-            else Console.WriteLine("Y or X = 0");
 
+            for (int i = 0; i < result.Length; i++)
+            {
+                ++w;
+            }
+            Console.WriteLine($"Result = {w}");
             //Output the result 
         }
         #endregion
@@ -226,12 +250,14 @@ namespace Lab1_5
             //Use string array for Morse code
             string[,] Dictionary_arr = new string[,] { { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
             { ".-   ", "-... ", "-.-. ", "-..  ", ".    ", "..-. ", "--.  ", ".... ", "..   ", ".--- ", "-.-  ", ".-.. ", "--   ", "-.   ", "---  ", ".--. ", "--.- ", ".-.  ", "...  ", "-    ", "..-  ", "...- ", ".--  ", "-..- ", "-.-- ", "--.. ", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----." }};
-            
+
+            Console.WriteLine("\n" + new string('*', 30));
             //Use ToCharArray() method for string to copy characters to Unicode character array
             //Use foreach loop for character array in which
             string[,] dictUnic;
             foreach (var w in Dictionary_arr)
             {
+
                 //w.ToCharArray() = new string[,];
             }
 
