2ba5ba0ed27e77a5e1dc836992f0f21d48170556
Devenko <48014225+Devenko@users.noreply.github.com>
2019-05-02 22:31:44 +0300

    Tried to solve lab2_2_2

--- Training/Lab2_2_cod_2/Box.cs
+++ Training/Lab2_2_cod_2/Box.cs
@@ -9,15 +9,81 @@ namespace Lab2_2_cod_2
     class Box
     {
         private int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
         private int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
         private int width;
+        public int Widht
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
         private int height;
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
 
+        private string symbol;
+        public string Symbol
+        {
+            get { return symbol; }
+            set { symbol = value; }
+        }
 
+        
         //1.  Implement public  auto-implement properties for start position (point position)
         //auto-implement properties for width and height of the box
         //and auto-implement properties for a symbol of a given set of valid characters (*, + ,.) to be used for the border 
         //and a message inside the box
+        public void Draw(int height, int width, int x, int y, string symbol)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (j == 0 || j == 14)
+                    {
+                        Console.SetCursorPosition(x + 2, y + 2);
+                        Console.Write("*");
+                    }
+                    else if (i == 0 || i == 4)
+                    {
+                        Console.SetCursorPosition(x + 2, y + 2);
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(x + 2, y + 2);
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Ширина: ");
+            height = int.Parse(Console.ReadLine());
+            Console.Write("Высота: ");
+            width = int.Parse(Console.ReadLine());
+            Console.Write("Отступ справа: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Отступ сверху: ");
+            y = int.Parse(Console.ReadLine());
+            Console.Write("Введите символ, каким рисовать прямоугольник: ");
+            symbol = Convert.ToString(Console.ReadLine());
+        }
 
         //2.  Implement public Draw() method
         //to define start position, width and height, symbol, message  according to properties
@@ -29,7 +95,31 @@ namespace Lab2_2_cod_2
         //Change the message in the method to return the Box square
         //Use Console.SetCursorPosition() method
         //Trim the message if necessary
-
+        private void Draw()
+        {
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    if (j == 0 || j == height - 1)
+                    {
+                        Console.SetCursorPosition(x + j, y + i);
+                        Console.Write(symbol);
+                    }
+                    else if (i == 0 || i == width - 1)
+                    {
+                        Console.SetCursorPosition(x + j, y + i);
+                        Console.Write(symbol);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(x + j, y + i);
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
 
     }
 }
--- Training/Lab2_2_cod_2/Program.cs
+++ Training/Lab2_2_cod_2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Lab2_2_cod_2;
 
 namespace Hello_Cons_Dr_Methods
 {
@@ -10,32 +12,58 @@ namespace Hello_Cons_Dr_Methods
     {
         static void Main(string[] args)
         {
-            //string s = "*";
-            //string space = " ";
-            //string temp = "";
-            int width = 0;
-            int height = 0;
-            Console.Write("Высота: ");
-            height = int.Parse(Console.ReadLine());
-            Console.Write("Ширина: ");
-            width = int.Parse(Console.ReadLine());
-            
+            //int width = 0;
+            //int height = 0;
+            //int x = 0, y = 0;
+            //string symbol;
 
-            for (int j = 0; j < height; j++)
-            {
-                for (int i = 0; i < width; i++)
-                {
-                    if (j == 0 || j == height - 1) Console.Write("*");
-                    else if (i == 0 || i == width - 1) Console.Write("*");
-                    else Console.Write(" ");
-                }
-                Console.WriteLine();
-            }
 
+            //Console.Write("Ширина: ");
+            //height = int.Parse(Console.ReadLine());
+            //Console.Write("Высота: ");
+            //width = int.Parse(Console.ReadLine());
+            //Console.Write("Отступ справа: ");
+            //x = int.Parse(Console.ReadLine());
+            //Console.Write("Отступ сверху: ");
+            //y = int.Parse(Console.ReadLine());
+            //Console.Write("Введите символ, каким рисовать прямоугольник: ");
+            //symbol = Convert.ToString(Console.ReadLine());
+            //// Console.SetWindowSize(130, 70); // установка ширины и высоты консоли
+
+            //Console.Clear();
+            //// Console.SetCursorPosition(2, 2);
+
+            //for (int j = 0; j < height; j++)
+            //{
+            //    for (int i = 0; i < width; i++)
+            //    {
+            //        if (j == 0 || j == height - 1)
+            //        {
+            //            Console.SetCursorPosition(x+j, y+i);
+            //            Console.Write(symbol);
+            //        }
+            //        else if (i == 0 || i == width - 1)
+            //        {
+            //            Console.SetCursorPosition(x+j,y+i);
+            //            Console.Write(symbol);
+            //        }
+            //        else
+            //        {
+            //            Console.SetCursorPosition(x+j,y+i);
+            //            Console.Write(" ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int wid = Console.WindowWidth; //ширина консоли
+            int hei = Console.WindowHeight; //высота консоли
+            Console.WriteLine($"высота{hei}  ширина{wid}");
             try
             {
                 //Implement start position, width and height, symbol, message input
-
+                Box box = new Box();
+                box.Draw(5,15,5,5,"+");
                 //Create Box class instance
 
                 //Use  Box.Draw() method
